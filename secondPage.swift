5445cb424f1c49aa583b9f11248a0342c47e4d3d
Yipeng Zhang <zyp5511@gmail.com>
2016-02-17 22:01:07 -0600

    add some features in the code add two pages and a pikachu

--- secondPage.swift
+++ secondPage.swift
@@ -1,28 +1,29 @@
 //
-//  ViewController.swift
+//  secondPage.swift
 //  HW1CS407
 //
-//  Created by ZhangYipeng on 16/2/14.
+//  Created by ZhangYipeng on 16/2/17.
 //  Copyright © 2016年 ZhangYipeng. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class secondPage: UIViewController {
+    
+    
+    
+    @IBOutlet var Photo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.Photo.image = UIImage(named: "pikachu_s_shadow_wallpaper_by_cmanciecko-d7nomi9")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-    @IBAction func startBtn(sender: AnyObject) { self.performSegueWithIdentifier("oneTotwo", sender: self)
-        
-    }
 }
-
